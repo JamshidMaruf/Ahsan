1c6dcbb80c0a0b985a0ed507b047f3174f8add6d
MuzaffarNurillaew <nurillaewmuzaffar@gmail.com>
2023-04-13 11:53:04 +0500

    fluent api conventions added to entities, used in DbContext and checked

--- src/Ahsan.Domain/Entities/User.cs
+++ src/Ahsan.Domain/Entities/User.cs
@@ -11,4 +11,6 @@ public class User : Auditable
     public string Email { get; set; }
     public string Password { get; set; }
     public UserRole Role { get; set; }
+
+    public ICollection<Company> Companies { get; set; }
 }
