6630511d1eaacb435ce06ab26dc038161c3232f3
KomronSharpist <komrondeveloper@gmail.com>
2023-04-13 12:15:35 +0500

    Added EmailVerification extension

---
+++ src/Ahsan.Domain/Entities/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace Ahsan.Domain.Entities;
+
+
+public class Message
+{
+    public string To { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+}
---
+++ src/Ahsan.Service/Helpers/EmailVerification.cs
@@ -0,0 +1,47 @@
+﻿using MailKit.Security;
+using MimeKit.Text;
+using MimeKit;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ahsan.Domain.Entities;
+
+namespace Ahsan.Service.Helpers;
+
+public class EmailVerification
+{
+    public Task<string> SendAsync(User user)
+    {
+        try
+        {
+            Random random = new Random();
+            int verificationCode = random.Next(123456, 999999);
+
+            ConnectionMultiplexer resdisConnect = ConnectionMultiplexer.Connect("localhost");
+            IDatabase db = resdisConnect.GetDatabase();
+            db.StringSet("code", verificationCode.ToString());
+            var result = db.StringGet("code");
+
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(this.configuration["EmailAddress"]));
+            email.To.Add(MailboxAddress.Parse(user.Email));
+            email.Subject = "Email verification Ahsan.uz";
+            email.Body = new TextPart(TextFormat.Html) { Text = result.ToString() };
+
+            var sendMessage = new MailKit.Net.Smtp.SmtpClient();
+            await sendMessage.ConnectAsync(this.configuration["Host"], 587, SecureSocketOptions.StartTls);
+            await sendMessage.AuthenticateAsync(this.configuration["EmailAddress"], this.configuration["Password"]);
+            await sendMessage.SendAsync(email);
+            await sendMessage.DisconnectAsync(true);
+
+            return Ok(result.ToString());
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
