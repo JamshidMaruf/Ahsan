eeb4cf3d49e5da8c80b765bcf4f1afc7bdb255e9
MuzaffarNurillaew <nurillaewmuzaffar@gmail.com>
2023-04-13 14:47:55 +0500

    PaginationParams model and extension method to paginate has been added

--- src/Ahsan.Domain/Configurations/PaginationParams.cs
+++ src/Ahsan.Domain/Configurations/PaginationParams.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ahsan.Domain.Configurations
+﻿namespace Ahsan.Domain.Configurations
 {
-    internal class PaginationParams
+    public class PaginationParams
     {
+        private const short _maxSize = 10;
+        private const short _minSize = 1;
+        private short _pageSize = 1;
+        private int _pageIndex = 1;
+        public short PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = value > _maxSize ? _maxSize : value < _minSize ? _minSize : value;
+        }
+
+        public int PageIndex
+        {
+            get => _pageIndex;
+            set => _pageIndex = value < 0 ? _pageIndex : value;
+        }
     }
 }
