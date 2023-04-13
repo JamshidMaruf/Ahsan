0eb4c08a078d2e19c4d0f7588c366fc94417dd43
azimochilov <azimochilov226@gmail.com>
2023-04-13 12:16:58 +0300

    fixed password hasher

--- src/Ahsan.Service/Helpers/PasswrodHasher.cs
+++ src/Ahsan.Service/Helpers/PasswrodHasher.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Ahsan.Service.Helpers;
 
-namespace Ahsan.Service.Helpers
+public class PasswrodHasher
 {
-    internal class PasswrodHasher
+    public static (string passwordHash, string salt) Hash(string password)
     {
+        string salt = Guid.NewGuid().ToString();
+        string strongpassword = salt + password;
+        string passwordHash = BCrypt.Net.BCrypt.HashPassword(strongpassword);
+        return (passwordHash, salt);
+    }
+
+    public static bool Verify(string password, string salt, string passwordHash)
+    {
+        string strongpassword = salt + password;
+        var result = BCrypt.Net.BCrypt.Verify(strongpassword, passwordHash);
+        return result;
     }
 }
