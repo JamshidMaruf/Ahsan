f9b3a52e913422ef250142a3d35c5cc9201337c3
Muxammad <muxammadcodes@gmail.com>
2023-04-15 02:08:47 +0500

    Exceptions folder transfered into commons F

---
+++ src/Ahsan.Service/Commons/Exceptions/AhsanException.cs
@@ -0,0 +1,12 @@
+﻿namespace Ahsan.Service.Commons.Exceptions
+{
+    public class AhsanException : Exception
+    {
+        public int Code { get; set; }
+        public AhsanException(int code, string message)
+            : base(message)
+        {
+            Code = code;
+        }
+    }
+}
