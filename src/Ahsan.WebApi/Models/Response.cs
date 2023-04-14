9d2e9349d5a64d21b581392ebfd3d8ca018180df
komron2052@gmail.com <komron2052@gmail.com>
2023-04-14 06:09:12 +0500

    Created middleware for requests and responses

--- src/Ahsan.WebApi/Models/Response.cs
+++ src/Ahsan.WebApi/Models/Response.cs
@@ -1,6 +1,10 @@
-﻿namespace Ahsan.WebApi.Models
+﻿#pragma warning disable
+
+namespace Ahsan.WebApi.Models;
+
+public class Response
 {
-    public class Response
-    {
-    }
+    public int Code { get; set; }
+    public object Data { get; set; }
+    public string Error { get; set; }
 }
