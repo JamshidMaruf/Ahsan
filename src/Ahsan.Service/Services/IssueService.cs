a244592a2f1a4ae74688bfa3bcfcc51b1218952e
wonderboy1w3 <wonderboy1w3@gmail.com>
2023-04-26 17:32:33 +0300

    commit

--- src/Ahsan.Service/Services/IssueService.cs
+++ src/Ahsan.Service/Services/IssueService.cs
@@ -7,21 +7,32 @@ using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
-namespace Ahsan.Service.Services
-{
-    public class IssueService : IIssueService
-    {
-        private readonly IRepository<Issue> _issueRepository;
-        private readonly IIssueCategoryService _issueCategoryService;
-        private readonly IUserService _userService;
-        private readonly IMapper _mapper;
+namespace Ahsan.Service.Services;
 
-        public IssueService(
-            IRepository<Issue> repository,
-            IIssueCategoryService issueCategoryService,
-            IUserService userService,
-            IMapper mapper
-            )
+public class IssueService : IIssueService
+{
+    private readonly IRepository<Issue> _issueRepository;
+    private readonly IIssueCategoryService _issueCategoryService;
+    private readonly IUserService _userService;
+    private readonly IMapper _mapper;
+
+    public IssueService(
+        IRepository<Issue> repository,
+        IIssueCategoryService issueCategoryService,
+        IUserService userService,
+        IMapper mapper
+        )
+    {
+        this._issueRepository = repository;
+        this._issueCategoryService = issueCategoryService;
+        this._userService = userService;
+        this._mapper = mapper;
+    }
+    public async ValueTask<IssueForResultDto> CreateAsync(IssueForCreationDto dto)
+    {
+        var issue = await this._issueRepository.GetAsync(u => u.Title == dto.Title);
+
+        if (issue is not null)
         {
             this._issueRepository = repository;
             this._issueCategoryService = issueCategoryService;
@@ -86,10 +97,21 @@ namespace Ahsan.Service.Services
                 c .Description.Contains(search) ||
                 c.Number.ToString() == search).ToListAsync();
 
+=======
+            throw new CustomException(403, "issue already exist");
+        }
+
+        var user = await this._userService.GetByIdAsync(dto.AssignedId);
+        var issueCategory = await this._issueCategoryService.GetByIdAsync(dto.CategoryId);
+        if (user is not null && issueCategory is not null)
+        {
+            Issue mappedUser = _mapper.Map<Issue>(dto);
             try
             {
-                var result = _mapper.Map<IEnumerable<IssueForResultDto>>(matchingIssues);
-                return result;
+                var result = await this._issueRepository.InsertAsync(mappedUser);
+                await this._issueRepository.SaveChangesAsync();
+
+                return this._mapper.Map<IssueForResultDto>(result);
             }
 
             catch
@@ -112,6 +134,7 @@ namespace Ahsan.Service.Services
             }
 
             catch
+            catch (Exception)
             {
                 throw new AhsanException(500, "Something went wrong");
             }
@@ -135,6 +158,84 @@ namespace Ahsan.Service.Services
             await this._issueRepository.SaveChangesAsync();
 
             return _mapper.Map<IssueForResultDto>(issue);
+        else
+        {
+            throw new CustomException(400, "Try again!");
         }
+    }
+
+    public async ValueTask<bool> DeleteAsync(long id)
+    {
+        var issue = await this._issueRepository.GetAsync(i => i.Id == id);
+
+        if (issue is null)
+        {
+            throw new CustomException(404, "Issue not found");
+        }
+
+        await _issueRepository.DeleteAsync(issue);
+
+        await this._issueRepository.SaveChangesAsync();
+
+        return true;
+    }
+
+    public async ValueTask<IEnumerable<IssueForResultDto>> GetAllAsync(Expression<Func<Issue, bool>> expression = null, string search = null)
+    {
+        var issues = this._issueRepository.GetAll(expression, new string[] { "IssueCategory", "Company", "CompanyEmployee" }, isTracking: false);
+
+        var matchingIssues = await issues.Where(
+            c => c.Title.ToLower() == search ||
+            c.Description.Contains(search)).ToListAsync();
+
+        try
+        {
+            var result = _mapper.Map<IEnumerable<IssueForResultDto>>(matchingIssues);
+            return result;
+        }
+
+        catch
+        {
+            throw new CustomException(500, "Something went wromg");
+        }
+    }
+
+    public async ValueTask<IssueForResultDto> GetByIdAsync(long id)
+    {
+        var issue = await this._issueRepository.GetAsync(i => i.Id == id);
+
+        if (issue is null)
+            throw new CustomException(404, "Issue not found");
+
+        try
+        {
+            var result = _mapper.Map<IssueForResultDto>(issue);
+            return result;
+        }
+
+        catch
+        {
+            throw new CustomException(500, "Something went wrong");
+        }
+    }
+
+    public async ValueTask<IssueForResultDto> UpdateAsync(IssueForUpdateDto dto)
+    {
+        var updatingIssue = await this._issueRepository.GetAsync(u => u.Id == dto.Id);
+
+        if (updatingIssue is null)
+        {
+            throw new CustomException(404, "Issue not found");
+        }
+
+        var issue = _mapper.Map<Issue>(dto);
+
+        issue.UpdatedAt = DateTime.UtcNow;
+
+        await this._issueRepository.UpdateAsync(issue);
+
+        await this._issueRepository.SaveChangesAsync();
+
+        return _mapper.Map<IssueForResultDto>(issue);
     }
 }
---
+++ src/Ahsan.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,132 @@
+﻿using Ahsan.Service.DTOs.Users;
+using Ahsan.Service.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ahsan.WebApi.Controllers;
+
+#pragma warning disable
+public class UsersController : BaseController
+{
+    private readonly IUserService userService;
+    public UsersController(IUserService userService)
+    {
+        this.userService = userService;
+    }
+
+    /// <summary>
+    /// Create user
+    /// </summary>
+    /// <param name="dto"></param>
+    /// <returns></returns>
+
+    [Authorize(Roles = "Admin")]
+    [HttpPost("create")]
+    public async Task<IActionResult> PostCompanyAsync(UserForCreationDto dto)
+       => Ok(new
+       {
+           Code = 200,
+           Error = "Success",
+           Data = await this.userService.CreateAsync(dto)
+       });
+
+    /// <summary>
+    /// Update user
+    /// </summary>
+    /// <param name="dto"></param>
+    /// <returns></returns>
+    [HttpPut("update")]
+    public async Task<IActionResult> PutCompanyAsync(UserForUpdateDto dto)
+        => Ok(new
+        {
+            Code = 200,
+            Error = "Success",
+            Data = await this.userService.UpdateAsync(dto)
+        });
+
+    /// <summary>
+    /// Delete user via given id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    
+    [Authorize(Roles = "Admin")]
+    [HttpDelete("delete/{id:long}")]
+    public async Task<IActionResult> DeleteCompany(long id)
+        => Ok(new
+        {
+            Code = 200,
+            Error = "Success",
+            Data = await this.userService.DeleteAsync(id)
+        });
+
+    /// <summary>
+    /// Get user for given id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpGet("get-by-id/{id:long}")]
+    public async Task<IActionResult> GetByIdAsync(long id)
+        => Ok(new
+        {
+            Code = 200,
+            Error = "Success",
+            Data = await this.userService.GetByIdAsync(id)
+        });
+
+    /// <summary>
+    /// Get user list
+    /// </summary>
+    /// <returns></returns>
+    [Authorize(Roles = "User")]
+    [HttpGet("get-list")]
+    public async Task<IActionResult> GetAllUsers()
+        => Ok(new
+        {
+            Code = 200,
+            Error = "Success",
+            Data = await this.userService.GetAllAsync()
+        });
+
+    /// <summary>
+    /// Upload user profile image
+    /// </summary>
+    /// <param name="dto"></param>
+    /// <returns></returns>
+    [HttpPost("image-upload")]
+    public async ValueTask<IActionResult> UploadImage([FromForm] UserImageForCreationDto dto)
+        => Ok(new
+        {
+            Code = 200,
+            Error = "Success",
+            Data = await this.userService.ImageUploadAsync(dto)
+        });
+
+    /// <summary>
+    /// Delete user profile image
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
+    [HttpDelete("image-delete/{userId:long}")]
+    public async Task<IActionResult> DeleteUserImage(long userId)
+        => Ok(new
+        {
+            Code = 200,
+            Error = "Success",
+            Data = await this.userService.DeleteUserImageAsync(userId)
+        });
+
+    /// <summary>
+    /// Get user profile image
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
+    [HttpGet("image-get/{userId:long}")]
+    public async Task<IActionResult> GetUserImage(long userId)
+        => Ok(new
+        {
+            Code = 200,
+            Error = "Success",
+            Data = await this.userService.GetUserImageAsync(userId)
+        });
+}
